d0ffb3b167311baa62e386bfbd169d192aca12f1
Maksimandr <maksimandr@mail.ru>
2021-05-16 21:21:36 +0500

    lesson 6 отправка сообщений идет, но в целом не работает как надо

---
+++ src/main/java/lesson6/ConsoleChatConstants.java
@@ -0,0 +1,11 @@
+package lesson6;
+
+public class ConsoleChatConstants {
+
+    public static final int PORT = 8189;
+
+    public static final String HOST = "localhost";
+
+    public static final String POISON_PILL = "end";
+
+}
