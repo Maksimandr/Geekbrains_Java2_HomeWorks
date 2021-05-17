9b249aa11ef4d1ab895516fe7848edf614647abc
Maksimandr <maksimandr@mail.ru>
2021-05-17 14:41:39 +0500

    lesson 6 теперь работает нормально, кажется :)

--- src/main/java/lesson6/ChatServer.java
+++ src/main/java/lesson6/ChatServer.java
@@ -1,20 +1,34 @@
 package lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
 
-    public ChatServer(){
-
+    public ChatServer() {
+        Socket socket = null;
         try (ServerSocket serverSocket = new ServerSocket(ConsoleChatConstants.PORT)) {
-            System.out.println("Сервер запущен, ожидаем подключения...");
-            Socket socket = serverSocket.accept();
-            System.out.println("Клиент подключился");
-            new ConsoleUI(socket);
-        } catch (IOException e) {
-            System.out.println("Ошибка инициализации сервера");
+            System.out.println("Server started. Wait for connection...");
+            socket = serverSocket.accept();
+            System.out.println("Client connected!");
+            new ConsoleUI(socket, "Server");
+
+//            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+//            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+//            while (true) {
+//                String string = inputStream.readUTF();
+//                if (string.equals(ConsoleChatConstants.POISON_PILL)) {
+//                    break;
+//                }
+//                System.out.println("Message received " + string);
+////                outputStream.writeUTF("Echo : " + string);
+//            }
+//            System.out.println("Server shutting down");
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
