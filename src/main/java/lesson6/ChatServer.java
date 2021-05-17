5844f25c72bb5d5c8736cb5e5c36dfd4a03a0fe8
Maksimandr <maksimandr@mail.ru>
2021-05-17 15:11:05 +0500

    lesson 6 добавил комментарии

--- src/main/java/lesson6/ChatServer.java
+++ src/main/java/lesson6/ChatServer.java
@@ -1,7 +1,5 @@
 package lesson6;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,24 +7,12 @@ import java.net.Socket;
 public class ChatServer {
 
     public ChatServer() {
-        Socket socket = null;
+        Socket socket;
         try (ServerSocket serverSocket = new ServerSocket(ConsoleChatConstants.PORT)) {
             System.out.println("Server started. Wait for connection...");
             socket = serverSocket.accept();
             System.out.println("Client connected!");
             new ConsoleUI(socket, "Server");
-
-//            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-//            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-//            while (true) {
-//                String string = inputStream.readUTF();
-//                if (string.equals(ConsoleChatConstants.POISON_PILL)) {
-//                    break;
-//                }
-//                System.out.println("Message received " + string);
-////                outputStream.writeUTF("Echo : " + string);
-//            }
-//            System.out.println("Server shutting down");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
